b7d7f80261ecb7b322ce40750fe219659254f9c8
r00t254 <100118426+r00t254@users.noreply.github.com>
2022-05-31 15:49:02 +0300

    Update settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -115,8 +115,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
